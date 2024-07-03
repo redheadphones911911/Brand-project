cc0b4127c1b8e8ed3df4ba69c19992f75fdafff1
redheadphones911911 <74419059+redheadphones911911@users.noreply.github.com>
2024-07-03 21:54:47 +0000

    u

--- brand/src/App.jsx
+++ brand/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import Navigation from './components/navigation.jsx';
+import HeroSection from './components/hero.jsx';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
   return (
 <div>
 <Navigation/>
+<HeroSection/>
 </div>
   )
 }
---
+++ brand/src/components/hero.jsx
@@ -0,0 +1,26 @@
+const HeroSection = () => {
+    return(
+      <main className="hero container">
+      <div className="hero-content">
+        <h1>spero spathis</h1>
+        <p>Top one cola in Egypt , for many years we are producing it only for local people because they are our men</p>
+        
+                <div className="hero-btn">
+                    <button>Shop Now</button>
+                    <button className="secondary-btn">Category</button>
+            </div>
+        <div className="shopping">
+            <p>Also Available On</p>
+        </div>
+        <div className="brand-icons">
+            <img/>
+            <img/>
+        </div>
+        </div>
+        <div className="hero-image">
+            <img/>
+        </div>
+        </main>
+    )
+}
+export default HeroSection;
